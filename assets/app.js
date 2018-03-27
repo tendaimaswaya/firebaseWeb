bc3184d465349834421fa0eb56bb71d07ca00673
Tendai Maswaya <maswayatm@gmail.com>
2018-03-27 14:16:38 +0200

    Add files via upload

---
+++ assets/app.js
@@ -0,0 +1,8 @@
+  var config = {
+    apiKey: "AIzaSyBz13eirmEOGD1uXOZwf6tKGnEsjfwsUFo",
+    authDomain: "platformtechproject.firebaseapp.com",
+    databaseURL: "https://platformtechproject.firebaseio.com",
+    storageBucket: "platformtechproject.appspot.com",
+    messagingSenderId: "1065758005439",
+  };
+  firebase.initializeApp(config);
